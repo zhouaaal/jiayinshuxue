eb371629656145a8d1969254f6ca61fd8ca2d941
xiaoyao090821 <xiao8829@vip.qq.com>
2015-03-31 10:21:35 +0800

    Update app.js

--- cloud/app.js
+++ cloud/app.js
@@ -88,7 +88,7 @@ app.get('/vote',function(req,res){
 });
 
 app.post('/',function(req, res){
-	var name = req.body.getelementbyid("name");
+	var name = req.body.name;
 	var number = req.body.number;
 	var count = req.body.count;
 	if(true){
